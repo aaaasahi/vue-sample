252868ad493730dd6b92e730101bfd3e90568683
aaaasahi <asapjorf1017@gmail.com>
2023-02-04 12:21:29 +0900

    ref reactive v-model

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,24 +1,49 @@
 <template>
   <div class="container">
     <h1>最近の支出</h1>
+    <!-- 同じ意味 -->
+    <input @input="input1" :value="item1.name">
+    <input v-model="item1.price">
+    <!-- 　　　　 -->
+    <button @click="clear">clear</button>
     <div class="payment">
-      <label>{{ item1 }}</label>
-      <label>{{ price1 }}</label>
+      <label>{{ item1.name }}</label>
+      <label>{{ item1.price }}</label>
       <a :href="url">{{ url }}</a>
       <button @click="click('クリック！')">button</button>
     </div>
   </div>
-
 </template>
 
 <script setup lang="ts">
+import { ref, reactive } from 'vue';
 
-const item1 = "コーヒー"
-const price1 = 300
+// 単一の値の場合はrefオブジェクトごとの場合はreactive
+
+// const item1 = ref<string>('a')
+
+const item1 = reactive({
+  name: '',
+  price: 0
+})
+
 const url = 'https://google.com'
 
 const click = (itemName: string) => {
   window.alert(itemName)
+}
+
+const input1 = (e: any) => {
+  item1.name = e.target.value
+}
+
+const input2 = (e: any) => {
+  item1.price = e.target.value
+}
+
+const clear = () => {
+  item1.name = '',
+  item1.price = 0
 }
 
 </script>
